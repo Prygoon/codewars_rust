019325ff90d244a24fac63a761ad3faa9d89de01
Qwerty <i@dont.know>
2022-03-28 11:09:23 +0300

    From pocketowl

--- sum_of_digits/src/main.rs
+++ sum_of_digits/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", digital_root(1234));
+}
+
+fn digital_root(n: i64) -> i64 {
+    let mut result: Vec<i64> = Vec::new();
+    let mut n_mut = n;
+    // result.push();
+
+    while n_mut > 0 {
+        result.push(n_mut % 10);
+        n_mut /= 10;
+    }
+
+    if result.len() > 1 {
+        digital_root(result.iter().sum())
+    } else {
+        result.iter().sum()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(digital_root(16), 7);
+    }
 }
